334ce756d6ea76870cf1064f01a34cb5fdfc6636
Frank <flockom@iit.edu>
2012-03-15 07:19:09 -0500

    plumbing finished... added a timestamp

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
+HADOOP_HOME=/opt/hadoop
+CLASSPATH=${HADOOP_HOME}/hadoop-core-1.0.1.jar:${HADOOP_HOME}/lib/commons-cli-1.2.jar
 .PHONY: clean plain
 
 WordCountJ: src/WordCountJ.java
 	javac src/WordCountJ.java -d bin/
+
+WordCountMR: src/WordCountMR.java	
+	javac -classpath ${CLASSPATH} -d bin src/WordCountMR.java
+	jar -cvf bin/WordCountMR.jar -C bin/ .
 
 # to run with two threads and results to out2.txt:
 # make plain N=2 OUT=out2.txt 
--- src/WordCountMR.java
+++ src/WordCountMR.java
@@ -1,7 +1,7 @@
 package org.apache.hadoop.examples;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -14,7 +14,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-public class WordCount {
+public class WordCountMR {
 
   public static class TokenizerMapper 
        extends Mapper<Object, Text, Text, IntWritable>{
@@ -49,6 +49,7 @@ public class WordCount {
   }
 
   public static void main(String[] args) throws Exception {
+      Date start = new Date();
     Configuration conf = new Configuration();
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
     if (otherArgs.length != 2) {
@@ -56,7 +57,7 @@ public class WordCount {
       System.exit(2);
     }
     Job job = new Job(conf, "word count");
-    job.setJarByClass(WordCount.class);
+    job.setJarByClass(WordCountMR.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
@@ -64,6 +65,8 @@ public class WordCount {
     job.setOutputValueClass(IntWritable.class);
     FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
     FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    boolean code = job.waitForCompletion(true);
+    System.out.println((new Date()).getTime()-start.getTime() + " total milliseconds");
+    System.exit( code ? 0 : 1);
   }
 }
